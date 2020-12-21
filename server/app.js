bb13c14809d08f10bf5a52a57a4bc99bb4de03ff
Varun Rampal <varun.rampall@gmail.com>
2020-12-21 15:41:14 -0800

    server-app-changes

--- server/app.js
+++ server/app.js
@@ -21,6 +21,10 @@ require('./startup/routes')(app);// Routes setup
 require('./startup/db')();// database setup
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+//const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+
+const server = app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+  });
 
 module.exports = server;
