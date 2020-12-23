60504a45f4b3d047f405825a0c03763809a3f4e2
Varun Rampal <varun.rampall@gmail.com>
2020-12-23 11:27:58 -0800

    server-changes

--- server/server.js
+++ server/server.js
@@ -22,9 +22,9 @@ require('./startup/db')();// database setup
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname+'/../build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(join(__dirname+'/../build/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 //const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
