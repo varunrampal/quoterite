93960e107403d8e2938c09a61febfea9df74f325
Varun Rampal <varun.rampall@gmail.com>
2020-12-26 08:37:48 -0800

    server-changes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { join } = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { json, urlencoded, Request, Response } = express;
 //const winston = require('winston');
 require('winston-mongodb');
+const winston = require('winston');
+const mongoose = require('mongoose');
+const {DB_CONNECTION_STRING} = require('./utils/constants');
 
 const app = express();
+const usersRoutes = require('./routes/users-routes');
 
 // get environment variabless
 // require('dotenv').config({
@@ -33,6 +41,32 @@ require('./startup/db')();// database setup
 // app.get('*', (req, res) => {
 //   res.sendFile(join(__dirname+'/../build/index.html'));
 // });
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+ app.use(json());
+ app.use(
+     urlencoded({
+         extended: false,
+     })
+ );
+ app.use(cookieParser());
+
+ app.use(cors());
+
+ // routes declaration
+ app.use('/api/user', usersRoutes); // => /api/user...
+
+ mongoose
+ .connect(DB_CONNECTION_STRING, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+     useFindAndModify: false,
+ })
+ .then(() => winston.info('Connected to MongoDB...'))
+ .catch((err) => {
+     console.log(err);
+ });
 
 const port = process.env.PORT || 5000;
 //const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
