a6e5f2a9d3d34c8739c73e8425b835b4d6702da7
Varun Rampal <varun.rampall@gmail.com>
2020-12-27 09:00:36 -0800

    routes-changes

--- server/server.js
+++ server/server.js
@@ -2,32 +2,32 @@ const express = require('express');
 const {
     join
 } = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const {
-    json,
-    urlencoded
-} = express;
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+// const {
+//     json,
+//     urlencoded
+// } = express;
 //const winston = require('winston');
 require('winston-mongodb');
-const winston = require('winston');
-const mongoose = require('mongoose');
-const {
-    DB_CONNECTION_STRING
-} = require('./utils/constants');
-const {
-    JWT_KEY
-} = require('./utils/constants');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const {
-    User
-} = require('./models/user');
-const {
-    success,
-    error
-} = require('./helpers/api-response');
+// const winston = require('winston');
+// const mongoose = require('mongoose');
+// const {
+//     DB_CONNECTION_STRING
+// } = require('./utils/constants');
+// const {
+//     JWT_KEY
+// } = require('./utils/constants');
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const {
+//     User
+// } = require('./models/user');
+// const {
+//     success,
+//     error
+// } = require('./helpers/api-response');
 const app = express();
 
 
@@ -42,18 +42,8 @@ const app = express();
 const buildPath = join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
-app.get('/jobs', async (req, res) => {
-    try {
-        res.send({
-            status: 'good'
-        });
-    } catch (error) {
-        res.status(400).send('Error while getting list of jobs.Try again later.');
-    }
-});
-
 require('./startup/logging')(); //logging setup
-//require('./startup/routes')(app);// Routes setup
+require('./startup/routes')(app);// Routes setup
 require('./startup/db')(); // database setup
 
 // The "catchall" handler: for any request that doesn't
@@ -62,22 +52,22 @@ require('./startup/db')(); // database setup
 //   res.sendFile(join(__dirname+'/../build/index.html'));
 // });
 
-app.use(bodyParser.json({
-    limit: '50mb'
-}));
-app.use(bodyParser.urlencoded({
-    limit: '50mb',
-    extended: true
-}));
-app.use(json());
-app.use(
-    urlencoded({
-        extended: false,
-    })
-);
-app.use(cookieParser());
+// app.use(bodyParser.json({
+//     limit: '50mb'
+// }));
+// app.use(bodyParser.urlencoded({
+//     limit: '50mb',
+//     extended: true
+// }));
+// app.use(json());
+// app.use(
+//     urlencoded({
+//         extended: false,
+//     })
+// );
+// app.use(cookieParser());
 
-app.use(cors());
+// app.use(cors());
 
 // routes declaration
 //app.use('/api/user', usersRoutes); // => /api/user...
@@ -96,69 +86,69 @@ app.use(cors());
 // @route POST /user/login
 // @desc to authenticate user, returns userid, email and token
 // @access Public
-app.post('/api/user/login', async (req, res, next) => {
-    try {
-        const {
-            email,
-            password
-        } = req.body;
+// app.post('/api/user/login', async (req, res, next) => {
+//     try {
+//         const {
+//             email,
+//             password
+//         } = req.body;
 
-        // check if user with the provided email-id exists
-        let existingUser;
+//         // check if user with the provided email-id exists
+//         let existingUser;
 
-        existingUser = await User.findOne({
-            email: email,
-            active: 1
-        });
+//         existingUser = await User.findOne({
+//             email: email,
+//             active: 1
+//         });
 
-        //user not exists with the provide email-id
-        if (!existingUser) {
+//         //user not exists with the provide email-id
+//         if (!existingUser) {
 
-            return res.status(401).json(error("Invalid credentials.", res.statusCode));
-        }
+//             return res.status(401).json(error("Invalid credentials.", res.statusCode));
+//         }
 
 
-        // check if provided password is correct( compare the password with the password saved in the database)
-        let isValidPassword = false;
+//         // check if provided password is correct( compare the password with the password saved in the database)
+//         let isValidPassword = false;
 
-        isValidPassword = await bcrypt.compare(password, existingUser.password);
+//         isValidPassword = await bcrypt.compare(password, existingUser.password);
 
-        // Password is not valid
-        if (!isValidPassword) {
+//         // Password is not valid
+//         if (!isValidPassword) {
 
-            return res.status(401).json(error("Invalid credentials.", res.statusCode));
-        }
+//             return res.status(401).json(error("Invalid credentials.", res.statusCode));
+//         }
 
-        // generate token
-        let token;
+//         // generate token
+//         let token;
 
-        token = jwt.sign({
-                userId: existingUser.id,
-                email: existingUser.email,
-            },
-            //process.env.JWT_KEY,
-            JWT_KEY, {
-                expiresIn: '1h',
-            }
-        );
+//         token = jwt.sign({
+//                 userId: existingUser.id,
+//                 email: existingUser.email,
+//             },
+//             //process.env.JWT_KEY,
+//             JWT_KEY, {
+//                 expiresIn: '1h',
+//             }
+//         );
 
-        res.status(200).json(
-            success(
-                'You are logged in successfully.', {
-                    userId: existingUser.id,
-                    userName: existingUser.name,
-                    userRole: existingUser.role,
-                    userEmail: existingUser.email,
-                    token: token,
-                },
-                res.statusCode
-            )
-        );
-    } catch (error) {
-        winston.error('Error while trying to login. Try again later.');
-        res.status(400).json(error("Error while trying to login. Try again later.", res.statusCode));
-    }
-});
+//         res.status(200).json(
+//             success(
+//                 'You are logged in successfully.', {
+//                     userId: existingUser.id,
+//                     userName: existingUser.name,
+//                     userRole: existingUser.role,
+//                     userEmail: existingUser.email,
+//                     token: token,
+//                 },
+//                 res.statusCode
+//             )
+//         );
+//     } catch (error) {
+//         winston.error('Error while trying to login. Try again later.');
+//         res.status(400).json(error("Error while trying to login. Try again later.", res.statusCode));
+//     }
+// });
 
 
 
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -2,17 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const { json, urlencoded, Request, Response } = express;
-const kue = require('kue');
+const { json, urlencoded} = express;
+//const kue = require('kue');
 const HttpError = require('../helpers/http-error');
 
 const error = require('../middleware/error');
 
 // import routes
 const usersRoutes = require('../routes/users-routes');
-const receiptRoutes = require('../routes/receipt-routes');
-const s3routes = require('../routes/s3-routes');
-const propertyRoutes = require('../routes/property-routes');
+// const receiptRoutes = require('../routes/receipt-routes');
+// const s3routes = require('../routes/s3-routes');
+// const propertyRoutes = require('../routes/property-routes');
 
 module.exports = function (app) {
    app.use(bodyParser.json({limit: '50mb'}));
@@ -29,10 +29,10 @@ module.exports = function (app) {
 
     // routes declaration
     app.use('/api/user', usersRoutes); // => /api/user...
-    app.use('/api/receipt', receiptRoutes); // => /api/receipt...
-    app.use('/api/sign_s3', s3routes); // => /api/sign_s3
-    app.use('/api/customer', propertyRoutes); // => /api/customer/properties...
-    app.use('/kue-api/', kue.app);
+    // app.use('/api/receipt', receiptRoutes); // => /api/receipt...
+    // app.use('/api/sign_s3', s3routes); // => /api/sign_s3
+    // app.use('/api/customer', propertyRoutes); // => /api/customer/properties...
+    // app.use('/kue-api/', kue.app);
 
     // handle 404 error(route not found)
     app.use((req, res, next) => {
