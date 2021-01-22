f2e23aff55cebecac1b78be2aeb17dfe8bbb808f
Varun Rampal <varun.rampall@gmail.com>
2021-01-22 13:56:59 -0800

    constants-server

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -2,8 +2,6 @@ const DB_CONNECTION_STRING = 'mongodb+srv://varun:mEIXDSFXyFnrJsEz@cluster0.9tow
 const JWT_KEY = 'vtnFsh96KfYskpLszJFC';
 const SENDGRID_API_KEY='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';
 const SENGRID_API_SENDER='varun.rampall@gmail.com';
-// const AWSAccessKeyId='AKIAXXV76Q2CYZDAOIX7';
-// const AWSSecretKey='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';
 const Bucket='quoterite';
 const sender_email = 'varun.rampall@gmail.com';
 const receiver_email = 'varun.rampall@gmail.com';
@@ -12,8 +10,6 @@ module.exports = {
     JWT_KEY,
     SENDGRID_API_KEY,
     SENGRID_API_SENDER,
-    // AWSAccessKeyId,
-    // AWSSecretKey,
     Bucket,
     sender_email,
     receiver_email
